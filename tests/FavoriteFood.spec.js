1df52b71ac584a6ee723f5251131bedd871801f2
mcglaser <michaelcharlesglaser@gmail.com>
2017-04-05 21:13:59 -0700

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -6,6 +6,22 @@ describe('Favorite Food Filter', function () {
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
 	}));
+    it('should filter on favoriteFood correctly', function () {
+       var mockedList = [{
+           name: 'Andrew',
+           favoriteFood: 'Italian'
+       }, {
+           name: 'Berry',
+           favoriteFood: 'French'
+       }, {
+           name: 'Kate',
+           favoriteFood: 'Chinese'
+       }];
 
+        var results = $filter('favoriteFood')(mockedList, 'Italian');
+
+        expect(results.length).toBe(1);
+        expect(results[0].name).toBe('Andrew');
+    });
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -3,8 +3,11 @@ describe('removeAllVowels Filter', function () {
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
 
+  it('should remove all vowels', function () {
+      expect(removeAllVowels('tesaeiout')).toEqual('tst');
+  }); 
 });
